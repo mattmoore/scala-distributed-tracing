70d436dda28f7fc33d02977e2e08bd1ca7224009
Matt Moore <matt@mattmoore.io>
2021-11-13 23:13:33 -0600

    Add kafka consumer.

--- user/src/main/scala/io/mattmoore/store/user/Main.scala
+++ user/src/main/scala/io/mattmoore/store/user/Main.scala
@@ -2,6 +2,7 @@ package io.mattmoore.store.user
 
 import cats.effect._
 import doobie._
+import fs2.kafka._
 import io.mattmoore.store.user.algebras._
 import io.mattmoore.store.user.domain._
 import io.mattmoore.store.user.repositories._
@@ -15,10 +16,18 @@ object Main extends IOApp {
       "org.postgresql.Driver",
       "jdbc:postgresql:user",
       "postgres",
-      ""
+      "password"
     )
     val userRepository: Repository[F, User] = new UserRepository(xa)
     val userService: UserService[F] = new UserServiceInterpreter[F](userRepository)
+
+    val consumerSettings =
+      ConsumerSettings[IO, String, String]
+        .withAutoOffsetReset(AutoOffsetReset.Earliest)
+        .withBootstrapServers("localhost:9092")
+        .withGroupId("group")
+
+    KafkaConsumer.stream(consumerSettings)
 
     IO(ExitCode.Success)
   }
