7d188cdecd1458b449c2a9a8d658e886ab22ae52
Matt Moore <matt@mattmoore.io>
2021-11-13 22:58:37 -0600

    Fix database container stuff in user service.

--- user/src/test/scala/io/mattmoore/store/user/services/UserServiceSuite.scala
+++ user/src/test/scala/io/mattmoore/store/user/services/UserServiceSuite.scala
@@ -2,7 +2,7 @@ package io.mattmoore.store.user.services
 
 import cats.effect._
 import cats.effect.unsafe.implicits.global
-import com.dimafeng.testcontainers.munit.TestContainersForAll
+import com.dimafeng.testcontainers.munit.TestContainersForEach
 import com.dimafeng.testcontainers.PostgreSQLContainer
 import doobie.util.transactor.Transactor
 import io.mattmoore.store.user.algebras._
@@ -10,68 +10,102 @@ import io.mattmoore.store.user.database._
 import io.mattmoore.store.user.domain._
 
 import java.util.UUID
+import org.flywaydb.core.Flyway
 
-class UserServiceSuite extends munit.FunSuite with TestContainersForAll {
+class UserServiceSuite extends munit.FunSuite with TestContainersForEach {
   override type Containers = PostgreSQLContainer
 
   override def startContainers(): PostgreSQLContainer = {
-    PostgreSQLContainer.Def().start()
+    val psql = PostgreSQLContainer.Def("postgres:14").start()
+    val flyway = Flyway
+      .configure()
+      .mixed(true)
+      .baselineOnMigrate(true)
+      .dataSource(psql.container.getJdbcUrl, psql.container.getUsername, psql.container.getPassword)
+      .load()
+      .migrate()
+    psql
   }
 
   type F[A] = IO[A]
 
-  val xa: Transactor[F] = Transactor.fromDriverManager[F](
-    "org.postgresql.Driver",
-    "jdbc:postgresql:user",
-    "postgres",
-    ""
-  )
-  val userRepository: Repository[F, User] = new UserRepository(xa)
-  val userService: UserService[F] = new UserServiceInterpreter[F](userRepository)
-
   test("getUser returns a user for the ID") {
-    val expected = User(
-      id = Some(UUID.fromString("32fe8628-4182-4900-9e52-b3c5304f97da")),
-      firstName = "Matt",
-      lastName = "Moore",
-      email = "matt@mattmoore.io",
-      address = "123 Anywhere Street, Chicago, IL"
-    )
-    val actual = userService.getUser(expected.id.get).unsafeRunSync()
-    assertEquals(actual, expected)
+
+    withContainers { case psql =>
+      val userRepository: Repository[F, User] = new UserRepository(
+        Transactor.fromDriverManager[F](
+          psql.container.getDriverClassName,
+          s"${psql.container.getJdbcUrl}/${psql.container.getDatabaseName}",
+          psql.container.getUsername,
+          psql.container.getPassword
+        )
+      )
+      val userService: UserService[F] = new UserServiceInterpreter[F](userRepository)
+      val expected = User(
+        id = Some(UUID.fromString("32fe8628-4182-4900-9e52-b3c5304f97da")),
+        firstName = "Matt",
+        lastName = "Moore",
+        email = "matt@mattmoore.io",
+        address = "123 Anywhere Street, Chicago, IL"
+      )
+      val actual = userService.getUser(expected.id.get).unsafeRunSync()
+      assertEquals(actual, expected)
+    }
   }
 
   test("addUser adds a user and returns the updated user record") {
-    val userToAdd = User(
-      firstName = "Matt",
-      lastName = "Moore",
-      email = "matt@mattmoore.io",
-      address = "123 Anywhere Street, Chicago, IL"
-    )
-    val expected = UUID.fromString("32fe8628-4182-4900-9e52-b3c5304f97da")
-    val actual = userService.addUser(userToAdd).unsafeRunSync()
-    assertEquals(actual, expected)
+    withContainers { case psql =>
+      val userRepository: Repository[F, User] = new UserRepository(
+        Transactor.fromDriverManager[F](
+          psql.container.getDriverClassName,
+          s"${psql.container.getJdbcUrl}/${psql.container.getDatabaseName}",
+          psql.container.getUsername,
+          psql.container.getPassword
+        )
+      )
+      val userService: UserService[F] = new UserServiceInterpreter[F](userRepository)
+      val userToAdd = User(
+        firstName = "Matt",
+        lastName = "Moore",
+        email = "matt@mattmoore.io",
+        address = "123 Anywhere Street, Chicago, IL"
+      )
+      val expected = UUID.fromString("32fe8628-4182-4900-9e52-b3c5304f97da")
+      val actual = userService.addUser(userToAdd).unsafeRunSync()
+      assertEquals(actual, expected)
+    }
   }
 
   test("updateUser updates an existing user and returns the updated user record") {
-    val initialUser = User(
-      firstName = "Matt",
-      lastName = "Moore",
-      email = "matt@mattmoore.io",
-      address = "123 Anywhere Street, Chicago, IL"
-    )
-    val userToUpdate = User(
-      id = Some(UUID.fromString("32fe8628-4182-4900-9e52-b3c5304f97da")),
-      firstName = "Matthew",
-      lastName = "Moore",
-      email = "matt@mattmoore.io",
-      address = "123 Anywhere Street, Chicago, IL"
-    )
+    withContainers { case psql =>
+      val userRepository: Repository[F, User] = new UserRepository(
+        Transactor.fromDriverManager[F](
+          psql.container.getDriverClassName,
+          s"${psql.container.getJdbcUrl}/${psql.container.getDatabaseName}",
+          psql.container.getUsername,
+          psql.container.getPassword
+        )
+      )
+      val userService: UserService[F] = new UserServiceInterpreter[F](userRepository)
+      val initialUser = User(
+        firstName = "Matt",
+        lastName = "Moore",
+        email = "matt@mattmoore.io",
+        address = "123 Anywhere Street, Chicago, IL"
+      )
+      val userToUpdate = User(
+        id = Some(UUID.fromString("32fe8628-4182-4900-9e52-b3c5304f97da")),
+        firstName = "Matthew",
+        lastName = "Moore",
+        email = "matt@mattmoore.io",
+        address = "123 Anywhere Street, Chicago, IL"
+      )
 
-    userService.addUser(initialUser).unsafeRunSync()
+      userService.addUser(initialUser).unsafeRunSync()
 
-    val expected = UUID.fromString("32fe8628-4182-4900-9e52-b3c5304f97da")
-    val actual = userService.updateUser(userToUpdate).unsafeRunSync()
-    assertEquals(actual, expected)
+      val expected = UUID.fromString("32fe8628-4182-4900-9e52-b3c5304f97da")
+      val actual = userService.updateUser(userToUpdate).unsafeRunSync()
+      assertEquals(actual, expected)
+    }
   }
 }
